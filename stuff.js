3b7bae36745eab038e88eec3b640df4aceff8e68
Tuuletallaja <masugota@gmail.com>
2020-03-04 22:24:00 +0200

    javascript push

--- stuff.js
+++ stuff.js
@@ -1,7 +1,15 @@
+var hourly
+var button
 
-function getValue() {
-    fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/58.3.1/all?startTime=2014&endTime=2014')
+function setOcupation(id) {
+    fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/'+ id +'.3.1/all?startTime=2014&endTime=2014')
     .then(response => {
+        if (button == undefined) {
+            button = document.getElementById(id)
+        }
+        window.button.style.backgroundColor = "#FFFCE7"
+        button = document.getElementById(id)
+        button.style.backgroundColor = "#8AE1FC"
         return response.json()
     })
     .then(data => {
@@ -9,14 +17,18 @@ function getValue() {
         hourly = hourly[Object.keys(hourly)[0]]
         hourly = hourly[Object.keys(hourly)[1]]
         hourly = hourly[0]
-        hourly = hourly[0]
-        return hourly
+        hourly = hourly[0].toFixed(2)
+        
+        var hrs = document.getElementById("hrs")
+
+        hrs.innerHTML = hourly
+        console.log(id)
 
     })
 }
 //window.onload = getValue
 
-function setOcupation () {
-    var id = document.get
-}
+//document.getElementsByClassName("job").addEventListener("click", setOcupation(this.id))
+
+
 
